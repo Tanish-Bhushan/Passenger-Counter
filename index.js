f0da8b6de7aa11b4acef4eaf9441a27f03a4782e
Tanish-Bhushan <92452986+Tanish-Bhushan@users.noreply.github.com>
2022-11-06 01:12:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-let saveEl = document.getElementById("save")
-let countEl = document.getElementById("entry")
-let count = 0
-
+let count=0
 function increment() {
-
-    count += 1
+    let countEl = document.getElementById("entry")
+    count=Number(countEl.textContent)
+    count+=1
     countEl.textContent = count
 }
 
 function decrement() {
-
+    let countEl = document.getElementById("entry")
     if (countEl.textContent > 0)
         countEl.textContent -= 1
 }
 
 function reload() {
-
+    let saveEl = document.getElementById("save")
+    let countEl = document.getElementById("entry")
     saveEl.textContent = "Previous Entries : "
-    countEl.textContent=0
+    countEl.textContent = 0
 }
 
 function save() {
-    let countStr = count + " - "
+    let saveEl = document.getElementById("save")
+    let countEl = document.getElementById("entry")
+    let countStr = countEl.textContent + " - "
     saveEl.textContent += countStr
     countEl.textContent = 0
     count = 0
