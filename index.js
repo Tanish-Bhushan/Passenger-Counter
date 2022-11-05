f5d51b775bd8b7c0353553352c41c6634b19b24a
Tanish-Bhushan <92452986+Tanish-Bhushan@users.noreply.github.com>
2022-11-05 17:46:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let saveEl = document.getElementById("save")
+let countEl = document.getElementById("entry")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
